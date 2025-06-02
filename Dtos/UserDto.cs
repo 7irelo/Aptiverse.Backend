bc8171f8705a6ab5373b3b5263e9bedbf3357864
7irelo <tirelo.eric@gmail.com>
2025-06-02 16:11:57 +0200

    Initial commit

---
+++ Dtos/UserDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Aptiverse.Backend.Dtos
+{
+    public class UserDto
+    {
+        public int Id { get; set; }
+        [Required] public int CategoryId { get; set; }
+        [Required] public int TypeId { get; set; }
+        [Required] [StringLength(50)] public string? FirstName { get; set; }
+        [StringLength(50)] public string? LastName { get; set; }
+    }
+}
