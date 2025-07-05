e1ed3f859d167a6f7810bcb11de7b8378d265457
7irelo <tirelo.eric@gmail.com>
2025-07-05 23:35:24 +0200

    Add UserTypeProfile for mapping UserType and UserTypeDto
    Introduced a new `UserTypeProfile` class in the
    `Aptiverse.Application.UserTypes.Mapping` namespace.
    This class sets up bidirectional mapping between
    `UserType` and `UserTypeDto` using AutoMapper.

---
+++ src/Aptiverse.Application/UserTypes/Mapping/UserTypeProfile.cs
@@ -0,0 +1,14 @@
+﻿using Aptiverse.Application.UserTypes.Dtos;
+using Aptiverse.Domain.Models;
+using AutoMapper;
+
+namespace Aptiverse.Application.UserTypes.Mapping
+{
+    public class UserTypeProfile : Profile
+    {
+        public UserTypeProfile()
+        {
+            CreateMap<UserType, UserTypeDto>().ReverseMap();
+        }
+    }
+}
