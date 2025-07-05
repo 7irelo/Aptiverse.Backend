a9829bfe978bdc40bd28e4cbd5490d21ef590110
7irelo <tirelo.eric@gmail.com>
2025-07-05 23:23:45 +0200

    Add EntityNotFoundException class for error handling
    Introduces a new `EntityNotFoundException` class in the
    `Aptiverse.Core.Exceptions` namespace. This exception inherits
    from the base `Exception` class and provides a constructor
    that accepts an entity name and key, formatting a message
    to indicate when an entity is not found.

---
+++ src/Aptiverse.Core/Exceptions/EntityNotFoundException.cs
@@ -0,0 +1,7 @@
+﻿namespace Aptiverse.Core.Exceptions
+{
+    public class EntityNotFoundException(string entityName, object key) 
+        : Exception($"Entity '{entityName}' with key '{key}' was not found.")
+    {
+    }
+}
