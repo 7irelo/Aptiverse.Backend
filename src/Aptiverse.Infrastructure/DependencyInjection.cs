93b564acefcefdb7a9b8d03277535c0788bc3612
7irelo <tirelo.eric@gmail.com>
2025-07-05 23:27:38 +0200

    Add DependencyInjection class for service configuration
    Introduces the `DependencyInjection` class in the `Aptiverse.Infrastructure` namespace.
    Adds the `AddInfrastructure` method to configure dependency injection for services including
    `ApplicationDbContext`, AutoMapper, a generic repository, and `IUserService`.
    Also includes necessary using directives for the new functionality.

---
+++ src/Aptiverse.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,29 @@
+﻿using Aptiverse.Application;
+using Aptiverse.Application.Users.Services;
+using Aptiverse.Domain.Interfaces;
+using Aptiverse.Infrastructure.Data;
+using Aptiverse.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Aptiverse.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseNpgsql(
+                    configuration.GetConnectionString("DefaultConnection"),
+                    b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+
+            services.AddAutoMapper(typeof(IAssemblyMarker).Assembly);
+
+            services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+            services.AddScoped<IUserService, UserService>();
+
+            return services;
+        }
+    }
+}
