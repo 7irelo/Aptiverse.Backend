0c07db98e38edded4c2cdbf135b37130fe8101e2
7irelo <tirelo.eric@gmail.com>
2025-07-05 23:31:43 +0200

    Add ApplicationDbContext with PostgreSQL support
    Introduces a new `ApplicationDbContext` class inheriting from `DbContext`, featuring DbSet properties for `User`, `UserType`, and `UserCategory`. Overrides `OnModelCreating` to apply configurations and set up PostgreSQL extensions for UUID and cryptographic functions. Configures concurrency tokens for entity properties and adds `ConfigureConventions` to limit string properties to a maximum length of 255 characters, ensuring they are not Unicode.

---
+++ src/Aptiverse.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,43 @@
+﻿using Aptiverse.Domain.Models;
+using Aptiverse.Infrastructure.Data.Configurations;
+using Microsoft.EntityFrameworkCore;
+
+namespace Aptiverse.Infrastructure.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserType> UserTypes { get; set; }
+        public DbSet<UserCategory> UserCategories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(UserConfiguration).Assembly);
+
+            if (Database.IsNpgsql())
+            {
+                modelBuilder.HasPostgresExtension("uuid-ossp");
+                modelBuilder.HasPostgresExtension("pgcrypto");
+
+                modelBuilder.Model.GetEntityTypes().ToList().ForEach(e =>
+                {
+                    modelBuilder.Entity(e.ClrType)
+                        .Property<uint>("xmin")
+                        .HasColumnName("xmin")
+                        .HasColumnType("xid")
+                        .ValueGeneratedOnAddOrUpdate()
+                        .IsConcurrencyToken();
+                });
+            }
+        }
+
+        protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+        {
+            configurationBuilder.Properties<string>()
+                .HaveMaxLength(255)
+                .AreUnicode(false);
+        }
+    }
+}
