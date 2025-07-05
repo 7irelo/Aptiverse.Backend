a3c69f964dc9c22f83961a2d6debb1490fca266b
7irelo <tirelo.eric@gmail.com>
2025-07-05 23:34:12 +0200

    Add UserProfile class for DTO to model mappings
    This commit introduces the `UserProfile` class in the
    `Aptiverse.Application.Users.Mapping` namespace, which
    utilizes AutoMapper to facilitate mappings between
    `UserDto` and `User`, `UserType` and `UserTypeDto`,
    as well as `UserCategory` and `UserCategoryDto`.
    The `Id` property of the `User` is configured to
    always map to `0` when creating a `User` from a
    `UserDto`, with support for reverse mapping.

---
+++ src/Aptiverse.Application/Users/Mapping/UserProfile.cs
@@ -0,0 +1,18 @@
+﻿using Aptiverse.Application.UserCategories.Dtos;
+using Aptiverse.Application.Users.Dtos;
+using Aptiverse.Application.UserTypes.Dtos;
+using Aptiverse.Domain.Models;
+using AutoMapper;
+
+namespace Aptiverse.Application.Users.Mapping
+{
+    public class UserProfile : Profile
+    {
+        public UserProfile()
+        {
+            CreateMap<UserDto, User>().ForMember(dest => dest.Id, opt => opt.MapFrom(_ => 0)).ReverseMap();
+            CreateMap<UserType, UserTypeDto>().ReverseMap();
+            CreateMap<UserCategory, UserCategoryDto>().ReverseMap();
+        }
+    }
+}
