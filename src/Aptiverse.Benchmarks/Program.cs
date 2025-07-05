71dad0f07bb025cd2374b8f8753bf93895a36261
7irelo <tirelo.eric@gmail.com>
2025-07-05 23:36:30 +0200

    Add benchmarking support in Program.cs
    This commit introduces a using directive for the
    BenchmarkDotNet.Running namespace and implements
    a Main method in the Program class to run benchmarks
    for the assembly. A commented-out line is also included
    to suggest running benchmarks for the UserRepositoryBenchmarks class.

---
+++ src/Aptiverse.Benchmarks/Program.cs
@@ -0,0 +1,11 @@
+﻿using BenchmarkDotNet.Running;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        BenchmarkRunner.Run(typeof(Program).Assembly);
+
+        // BenchmarkRunner.Run<UserRepositoryBenchmarks>();
+    }
+}
