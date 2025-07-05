079c027fd61dbb0b19c182a94277e4e145ed6f89
7irelo <tirelo.eric@gmail.com>
2025-07-05 23:33:17 +0200

    Add UserDto class for user data serialization
    Introduces the `UserDto` class in the `Aptiverse.Application.Users.Dtos` namespace. This class is marked with the `[DataContract]` attribute and includes properties for user information such as `Id`, `FirstName`, `LastName`, `Email`, `PhoneNumber`, `Password`, `TypeId`, and `CategoryId`. Key properties are marked with `[DataMember]`, and `Id` is read-only for Swagger documentation purposes.

---
+++ src/Aptiverse.Application/Users/Dtos/UserDto.cs
@@ -0,0 +1,19 @@
+﻿using Swashbuckle.AspNetCore.Annotations;
+using System.Runtime.Serialization;
+
+namespace Aptiverse.Application.Users.Dtos
+{
+    [DataContract]
+    public class UserDto
+    {
+        [DataMember(Name = "id")][SwaggerSchema(ReadOnly = true)] public long Id { get; set; }
+        [DataMember(Name = "firstName")] public required string FirstName { get; set; }
+        [DataMember(Name = "lastName")] public required string LastName { get; set; }
+        [DataMember(Name = "email")] public string? Email { get; set; }
+        [DataMember(Name = "phoneNumber")] public string? PhoneNumber { get; set; }
+        [DataMember(Name = "password")] public required string Password { get; set; }
+        [DataMember(Name = "typeId")] public int TypeId { get; set; }
+        [DataMember(Name = "categoryId")] public int CategoryId { get; set; }
+    }
+
+}
